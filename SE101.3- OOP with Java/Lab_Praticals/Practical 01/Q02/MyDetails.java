3ba00b58f59c8dd73a4f5c3f28a8f44d19d039e8
ChathuraKarunarathna <chathurakarunarathna01@gmail.com>
2023-07-28 00:08:49 +0530

    Update MyDetails.java

--- SE101.3- OOP with Java/Lab_Praticals/Practical 01/Q02/MyDetails.java
+++ SE101.3- OOP with Java/Lab_Praticals/Practical 01/Q02/MyDetails.java
@@ -1,6 +1,6 @@
 public class MyDetails {
     public static void main(String[] args) {
-        System.out.println("Chamith Weerasingha\nBSc(Hons)Software Engineering");
+        System.out.println("Chathura Karunarathna\nBSc(Hons)Software Engineering");
 
     }
 }
