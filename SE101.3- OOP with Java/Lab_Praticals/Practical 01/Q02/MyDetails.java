c8478660dff11b67a844835719d33a03c1ede0f6
ChathuraKarunarathna <chathurakarunarathna01@gmail.com>
2023-07-28 00:17:53 +0530

    First Commit

--- SE101.3- OOP with Java/Lab_Praticals/Practical 01/Q02/MyDetails.java
+++ SE101.3- OOP with Java/Lab_Praticals/Practical 01/Q02/MyDetails.java
@@ -1,6 +1,6 @@
 public class MyDetails {
     public static void main(String[] args) {
-        System.out.println("Chathura Karunarathna\nBSc(Hons)Software Engineering");
+        System.out.println("Chathura Deshan\nBSc(Hons)Software Engineering");
 
     }
 }
