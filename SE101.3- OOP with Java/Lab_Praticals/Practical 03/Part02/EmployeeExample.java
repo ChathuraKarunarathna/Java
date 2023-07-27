513380830d8bab3160075c7b5c5655a268288277
ChathuraKarunarathna <chathurakarunarathna01@gmail.com>
2023-07-28 00:21:02 +0530

    First Commit

--- SE101.3- OOP with Java/Lab_Praticals/Practical 03/Part02/EmployeeExample.java
+++ SE101.3- OOP with Java/Lab_Praticals/Practical 03/Part02/EmployeeExample.java
@@ -1,7 +1,7 @@
 public class EmployeeExample {
         public static void main(String[] args)
         {
-            Employee E1 = new Employee("Chamith",80000.00,10000.00);
+            Employee E1 = new Employee("Chathura",80000.00,10000.00);
 
             System.out.println("Employee Name:"+E1.getname());
             System.out.println("Salary:"+E1.getsalary());
